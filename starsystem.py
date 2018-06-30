e67d50dc7ea5b5e3e1acd220a7c6a01b7f634fea
ajul <ajul1987@gmail.com>
2018-06-30 01:52:30 -0700

    shop map

--- starsystem.py
+++ starsystem.py
@@ -72,14 +72,18 @@ scale = output_size_x / size_x
 
 output_size_y = int(round(scale * size_y * extra_scale_y))
 
-result_image = Image.new('RGB', (output_size_x, output_size_y))
-result_draw = ImageDraw.Draw(result_image)
+political_image = Image.new('RGB', (output_size_x, output_size_y))
+political_draw = ImageDraw.Draw(political_image)
+
+shop_image = Image.new('RGB', (output_size_x, output_size_y))
+shop_draw = ImageDraw.Draw(shop_image)
+
+difficulty_image = Image.new('RGB', (output_size_x, output_size_y))
+difficulty_draw = ImageDraw.Draw(difficulty_image)
 
 result_imagemap = '<imagemap>\n'
-result_imagemap += 'Image:Starsystem.png' # add other image stuff here
+result_imagemap += 'Image:Starsystem.png|frame' # add other image stuff here
 result_imagemap += '|Click on a system to visit the corresponding article.\n\n'
-
-
 
 def to_output_coords(x, y):
     output_x = (x - min_x) * scale
@@ -91,15 +95,57 @@ for data in datas:
     
     x, y = to_output_coords(data["Position"]["x"], data["Position"]["y"])
 
-    color = faction_colors[data["Owner"]]
+    political_color = faction_colors[data["Owner"]]
+
     
-    result_draw.ellipse([
+
+    tags = data["Tags"]["items"]
+
+    if 'planet_other_empty' in tags:
+        shop_color = (31, 31, 31)
+    elif 'planet_industry_manufacturing' not in tags:
+        shop_color = (63, 63, 63)
+    elif 'planet_industry_rich' not in tags:
+        shop_color = (63, 127, 63)
+    elif 'planet_industry_research' not in tags:
+        shop_color = (63, 63, 255)
+    elif 'planet_other_starleague' not in tags:
+        shop_color = (255, 63, 255)
+    else:
+        shop_color = (255, 255, 0)
+        
+    shop_color = tuple(shop_color)
+
+    difficulty = data["Difficulty"]
+    if difficulty == 0:
+        difficulty_color = (127, 127, 127)
+    elif difficulty > 0:
+        difficulty_color = (127 + difficulty * 42, 127 - difficulty * 42, 127 - difficulty * 42)
+    elif difficulty < 0:
+        difficulty_color = (127 + difficulty * 42, 127 + difficulty * 42, 127 - difficulty * 42)
+
+    name = data["Description"]["Name"]
+    
+    political_draw.ellipse([
         x - star_radius,
         y - star_radius,
         x + star_radius,
-        y + star_radius], fill=color)
+        y + star_radius], fill=political_color)
 
-    name = data["Description"]["Name"]
+    shop_draw.ellipse([
+        x - star_radius,
+        y - star_radius,
+        x + star_radius,
+        y + star_radius], fill=shop_color)
+
+    difficulty_draw.ellipse([
+        x - star_radius,
+        y - star_radius,
+        x + star_radius,
+        y + star_radius], fill=difficulty_color)
+
+    if 'planet_other_starleague' in tags:
+        shop_draw.text((x - 2, y - 5), 'L', font=font, fill=(0,0,0))
 
     system_id = data["Description"]["Id"]
     if system_id in text_offsets:
@@ -110,13 +156,17 @@ for data in datas:
         text_x = x + 1.5 * star_radius
         text_y = y - 0.5 * fontsize
 
-    result_draw.text((text_x, text_y), name, font=font, fill=color)
+    political_draw.text((text_x, text_y), name, font=font, fill=political_color)
+    shop_draw.text((text_x, text_y), name, font=font, fill=shop_color)
+    difficulty_draw.text((text_x, text_y), name, font=font, fill=difficulty_color)
 
     result_imagemap += 'circle %d %d %d [[%s]]\n' % (x, y, imagemap_radius, name)
 
-result_image.save('starsystem.png')
+political_image.save('starsystem.png')
+shop_image.save('starsystem_shops.png')
+difficulty_image.save('starsystem_difficulty.png')
 
-result_imagemap += '</imagemap>\n'
+result_imagemap += '</imagemap><br clear=all />\n'
 
 f = open('starsystem.txt', 'w')
 f.write(result_imagemap)
